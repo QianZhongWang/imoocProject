5363421a610458904cc5cb371882331337cc658f
qzw <1870271825@qq.com>
2023-11-27 14:12:00 +0800

    自动类型转换

---
+++ src/com/imooc/TypeExchangeTest.java
@@ -0,0 +1,20 @@
+package com.imooc;
+
+public class TypeExchangeTest {
+    public static void main(String[] args) {
+        // 1、定义一个整型字面量67832，赋值给char类型的变量c，并输出
+        int n = 67832;
+        char c = (char) n;
+        System.out.println("c = " + c);
+
+        // 定义一个整型变量n1，赋值为65，并输出
+        int n1 = 65;
+        System.out.println("n1 = " + n1);
+
+        // 定义一个字符型变量c1，赋值为n，并输出c1的值。
+
+
+
+
+    }
+}
