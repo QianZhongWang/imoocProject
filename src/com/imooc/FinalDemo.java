f53a61cbaeb02e1448442568ebfa88cb1126757c
qzw <1870271825@qq.com>
2023-11-27 15:52:25 +0800

    常量

---
+++ src/com/imooc/FinalDemo.java
@@ -0,0 +1,10 @@
+package com.imooc;
+
+public class FinalDemo {
+    public static void main(String[] args) {
+        int m = 5;
+        final int N = 6;
+        m = 10;
+        // N = 10; // 不能对final修饰的变量进行修改
+    }
+}
