5e2fa0f73966db6dba1d6de6d9768ef69ecf2b29
qzw <1870271825@qq.com>
2023-11-27 10:32:17 +0800

    增加字符类型的变量

---
+++ src/com/imooc/CharDemo.java
@@ -0,0 +1,27 @@
+package com.imooc;
+
+public class CharDemo {
+    public static void main(String[] args) {
+        // 定义一个变量存放字符'a'
+        char a = 'a';
+        System.out.println("a = " + a);
+
+        char ch = 65;
+        System.out.println("ch = " + ch);
+        // 整型可以与字符型进行转换的，整型对应的ASCII码值就是对应的字符
+
+
+        /*
+        * 1、定义一个字符型变量c 赋值为'S'并输出
+        * 2、定义一个字符型变量c1 赋值为78并输出
+        * */
+        char c = 'S';
+        System.out.println("c = " + c);
+        char c1 = 78;
+        System.out.println("c1 = " + c1);
+
+
+
+
+    }
+}
