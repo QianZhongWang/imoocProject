2029e023a8ef7b0f489b785d1c884c12d0071301
qzw <1870271825@qq.com>
2023-11-28 09:37:15 +0800

    条件判断的demo1

---
+++ src/com/imooc/operator/ConditionDemo2.java
@@ -0,0 +1,14 @@
+package com.imooc.operator;
+
+public class ConditionDemo2 {
+    public static void main(String[] args) {
+        // 判断一个整数是奇数还是偶数
+        int n = 10;
+        if (n % 2 == 0) {
+            System.out.println(n + "是偶数");
+        } else {
+            System.out.println(n + "是奇数");
+        }
+    }
+
+}
