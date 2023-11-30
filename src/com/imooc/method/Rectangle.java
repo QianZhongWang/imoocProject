05dc01213d188b486919d3f6d0b9f7fde844fb2f
qzw <1870271825@qq.com>
2023-11-30 10:54:16 +0800

    methodDemo

---
+++ src/com/imooc/method/Rectangle.java
@@ -0,0 +1,17 @@
+package com.imooc.method;
+
+public class Rectangle {
+    // 求长方形面积的方法 无参有返回值
+    public int getArea() {
+        int length = 5;
+        int width = 4;
+        return length * width;
+    }
+
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        int area = rectangle.getArea();
+        System.out.println("长方形的面积为：" + area);
+    }
+}
