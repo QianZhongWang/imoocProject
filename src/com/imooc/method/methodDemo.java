eb5093eee949f80170eb105ec6ab503d4759b1f9
qzw <1870271825@qq.com>
2023-11-30 10:45:54 +0800

    methodDemo

---
+++ src/com/imooc/method/methodDemo.java
@@ -0,0 +1,21 @@
+package com.imooc.method;
+
+public class methodDemo {
+    public static void main(String[] args) {
+        // 调用printStar()方法
+        printStar();
+        System.out.println("Hello World!");
+        // 调用printStar()方法
+        printStar();
+
+    }
+
+    /*
+     * 打印输出星号的方法
+     * 无参数，无返回值
+     */
+    public static void printStar() {
+        System.out.println("********************");
+    }
+
+}
